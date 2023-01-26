bb1040f2324ad1b88e0cd37be11564549af1f248
@nelly12345gok <@nelly12345gok@gmail.com>
2023-01-26 16:36:20 -0600

    add image packer

---
+++ packer_image_template/script.sh
@@ -0,0 +1,9 @@
+echo "Instaling Terraform using the latest"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor |  tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && sudo apt install terraform
+
+echo "Installing Ansible"
+sudo apt install ansible -y
+
+
